9a6cecd8a640d706d5434d41e30a365b829303b4
Senenatamnii <senenatamnii@gmail.com>
2025-04-26 10:51:42 +0300

    Updating Every Files from Ejazz APP

---
+++ src/test/java/com/senen/ejaz2/Ejaz2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senen.ejaz2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejaz2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
