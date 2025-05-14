86909d92df7f5f3c82a21ef260af6986dbf361ed
Khutaba <fahed.khutaba@kla.com>
2025-05-14 15:23:48 +0300

    adding post service skelaton

--- post-service/src/main/java/com/ejazz/post_service/PostServiceApplication.java
+++ post-service/src/main/java/com/ejazz/post_service/PostServiceApplication.java
@@ -1,13 +1,13 @@
-package com.ejazz.user_service;
+package com.ejazz.post_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserServiceApplication {
-//
+public class PostServiceApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
+		SpringApplication.run(PostServiceApplication.class, args);
 	}
 
 }
--- post-service/src/test/java/com/ejazz/post_service/PostServiceApplicationTests.java
+++ post-service/src/test/java/com/ejazz/post_service/PostServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ejazz.user_service;
+package com.ejazz.post_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserServiceApplicationTests {
+class PostServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
