1583f05fc80fe1a2083f07a9598dfdb417341e4c
Khutaba <fahed.khutaba@kla.com>
2025-05-17 10:03:09 +0300

    adding documentation and update week 01 tasks

--- account-service/src/main/java/com/ejazz/account_service/dto/AccountDTO.java
+++ account-service/src/main/java/com/ejazz/account_service/dto/AccountDTO.java
@@ -3,10 +3,12 @@ package com.ejazz.account_service.dto;
 
 import jakarta.validation.constraints.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
-@Data
+@Data @Getter @Setter
 public class AccountDTO {
 
     private String accountNumber; // No validation since it will be generated
--- account-service/src/main/java/com/ejazz/account_service/repository/AccountRepository.java
+++ account-service/src/main/java/com/ejazz/account_service/repository/AccountRepository.java
@@ -1,9 +1,13 @@
 package com.ejazz.account_service.repository;
 
 import com.ejazz.account_service.entity.Account;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
     boolean existsByEmail(String email);
     boolean existsByPhoneNumber(String phoneNumber);
+     Optional<Account> findByEmail(String email);
 }
