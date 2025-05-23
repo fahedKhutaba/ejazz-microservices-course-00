e7bd32cfedae3e86331c8466b7f786e37fad07d5
Khutaba <fahed.khutaba@kla.com>
2025-05-23 20:39:39 +0300

    renaming the config-server module adding integration test and adding integration test module and defining deployment folder

---
+++ integration-tests/src/test/java/com/ejazz/integration_tests/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.ejazz.integration_tests;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
